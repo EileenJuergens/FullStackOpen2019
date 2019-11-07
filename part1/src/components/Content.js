7505f633f012f42997a7e68201354210687bd3aa
EileenJuergens <juergens.eileen@gmail.com>
2019-11-07 14:54:54 +0100

    finished part1a

---
+++ part1/src/components/Content.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Part = (props) => {
+  return (
+    <>
+      <p>{props.name} {props.number}</p>
+    </>
+  )
+}
+
+
+const Content = (props) => {
+  return (
+    <>
+      <Part name={props.part1} number={props.exercises1}/>
+      <Part name={props.part2} number={props.exercises2}/>
+      <Part name={props.part3} number={props.exercises3}/>
+    </>
+  );
+}
+
+export default Content;
