e8221b3ffcdd5fe1bb202d868c7132e10ccda5e1
EileenJuergens <juergens.eileen@gmail.com>
2019-11-11 22:38:04 +0100

    finish part1.6-1.10

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>{text}</button>
+)
+
+const Statistic = ({text, value}) => (<p>{text} {value}</p>)
+
+const Statistics = ({good, neutral, bad}) => {
+  if (!good && !neutral && !bad){
+    return <p>No feedback given yet.</p>
+  }
+  return (
+    <div>
+      <Statistic text="good" value={good}/>
+      <Statistic text="neutral" value={neutral}/>
+      <Statistic text="bad" value={bad}/>
+      <Statistic text="all" value={good+neutral+bad}/>
+      <Statistic text="average" value={(good*1+neutral*0+bad*-1)/(good+neutral+bad)}/>
+      <Statistic text="positive percent" value={`${(100*good)/(good+neutral+bad)} %`}/>
+    </div>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => { 
+    setGood(good + 1)
+  }
+  const handleNeutralClick = () => { 
+    setNeutral(neutral + 1)
+  }
+  const handleBadClick = () => { 
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <h2>Give Feedback</h2>
+      <Button onClick={handleGoodClick} text='good'/>
+      <Button onClick={handleNeutralClick} text='neutral'/>
+      <Button onClick={handleBadClick} text='bad'/>
+      <h2>The Statistics</h2> 
+      <Statistics feedbackGiven={false} good={good} neutral={neutral} bad={bad}/>
+      {/* <Statistic text="good" value={good}/>
+      <Statistic text="neutral" value={neutral}/>
+      <Statistic text="bad" value={bad}/>
+      <Statistic text="all" value={good+neutral+bad}/>
+      <Statistic text="average" value={(good*1+neutral*0+bad*-1)/(good+neutral+bad)}/>
+      <Statistic text="positive percent" value={(100*good)/(good+neutral+bad)}/> */}
+    </div>
+  )
+}
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
