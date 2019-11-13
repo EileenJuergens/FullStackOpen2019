cc7782f5a67cddfb87db460e285b21928cb5665b
EileenJuergens <juergens.eileen@gmail.com>
2019-11-13 20:38:14 +0100

    finished part2.2

--- part2/courseContents/src/AtomComponents/MoleculeComponents/Content.js
+++ part2/courseContents/src/AtomComponents/MoleculeComponents/Content.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import Part from './OrganismComponents/Part'
+import Total from './OrganismComponents/Total'
 
 const Content = ({parts}) => {
 
   const lines = () => parts.map(part => <Part key={part.id} part={part}/>)
   
+
   return (
-    <div>
-      {lines()}
-    </div>
+    <>
+      <div>{lines()}</div>
+      <h4>Total of {parts[0].exercises + parts[1].exercises + parts[2].exercises + parts[3].exercises} exercises</h4>
+      <Total/>
+    </>
   )
 }
 
---
+++ part2/courseContents/src/AtomComponents/MoleculeComponents/OrganismComponents/Total.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Total = (props) => {
+  // console.log('props', props)
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Total;
