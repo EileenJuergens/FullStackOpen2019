b1f1a03de9ed79d2c5cfbc8e9aa2fdc1afa4016d
EileenJuergens <juergens.eileen@gmail.com>
2020-02-01 14:05:01 +0100

    finshed part 2.13

--- part2/countries/src/components/Countries.js
+++ part2/countries/src/components/Countries.js
@@ -1,32 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Countries = ({ searchResults, countries }) => {
-  
-  const rows = () => {
-    if (searchResults.length > 10) {
-      return <p>To many matches, specify another filter</p>
-    }
-    else if (searchResults.length > 1 && searchResults.length <= 10) {
-      return searchResults.map(country => <p key={country.name}>{country.name}</p>)
-    }
-    else if (searchResults.length === 1) {
-      return (searchResults.map(country => (
-        <div key={country.name}>
-          <h1>{country.name}</h1>
-          <p>capital {country.capital}</p>
-          <p>population {country.population}</p>
-          <h2>languages</h2>
-          {country.languages.map(language => <li key={language.name}>{language.name}</li>)}
-          <img src={country.flag} alt={country.name} style={{width: '150px'}}/>
-        </div>
-      )));
-    }
+
+const Countries = ({ searchResults }) => {
+  const [ countrys, setCountrys ] = useState(searchResults);
+
+  useEffect(() => {
+    setCountrys(searchResults);
+  }, [searchResults])
+
+  const handleShowOnClick = (country) => {
+    setCountrys([country])
   }
 
-  
-  // show country with:
-  // name, capital, population, languages, flag
+  const shortCountry = (country) => {
+    return (
+      <div key={country.name}>
+        <p>{country.name} <button 
+          onClick={() => handleShowOnClick(country)}>show</button></p>
+      </div>
+    )
+  }
 
+  const longCountry = (country) => {
+    return (
+      <div key={country.name}>
+        <h1>{country.name}</h1>
+        <p>capital {country.capital}</p>
+        <p>population {country.population}</p>
+        <h2>languages</h2>
+        {country.languages.map(language => <li key={language.name}>{language.name}</li>)}
+        <img src={country.flag} alt={country.name} style={{ width: '150px' }} />
+      </div>
+    )
+  }
+
+  const rows = () => {
+    if (countrys.length > 10) {
+      return <p>To many matches, specify another filter</p>
+    }
+    else if (countrys.length > 1 && countrys.length <= 10) {
+      return countrys.map(country => shortCountry(country))
+    }
+    else if (countrys.length === 1) {
+      return longCountry(countrys[0]);
+    }
+  }
 
   return (
     <>
