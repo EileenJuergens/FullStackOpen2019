a5dcfa8e9d2b311552182cd2142af86724f8ea73
EileenJuergens <juergens.eileen@gmail.com>
2020-03-14 17:29:52 +0100

    finish part 2.14

--- part2/countries/src/components/Countries.js
+++ part2/countries/src/components/Countries.js
@@ -1,12 +1,22 @@
 import React, { useState, useEffect } from 'react';
-
+import axios from 'axios';
 
 const Countries = ({ searchResults }) => {
-  const [ countrys, setCountrys ] = useState(searchResults);
+  const [countrys, setCountrys] = useState(searchResults);
+  const [weather, setWeather] = useState({})
 
   useEffect(() => {
     setCountrys(searchResults);
   }, [searchResults])
+
+  useEffect(() => {
+    const API_KEY = '...'
+    if (countrys.length === 1) {
+      axios
+        .get(`http://api.weatherstack.com/current?access_key=${API_KEY}&query=${countrys[0].capital}`)
+        .then(response => setWeather(response.data.current))
+    }
+  }, [countrys])
 
   const handleShowOnClick = (country) => {
     setCountrys([country])
@@ -15,7 +25,7 @@ const Countries = ({ searchResults }) => {
   const shortCountry = (country) => {
     return (
       <div key={country.name}>
-        <p>{country.name} <button 
+        <p>{country.name} <button
           onClick={() => handleShowOnClick(country)}>show</button></p>
       </div>
     )
@@ -25,11 +35,12 @@ const Countries = ({ searchResults }) => {
     return (
       <div key={country.name}>
         <h1>{country.name}</h1>
-        <p>capital {country.capital}</p>
-        <p>population {country.population}</p>
-        <h2>languages</h2>
+        <p>Capital: {country.capital}</p>
+        <p>Population: {country.population}</p>
+        <h2>Languages:</h2>
         {country.languages.map(language => <li key={language.name}>{language.name}</li>)}
         <img src={country.flag} alt={country.name} style={{ width: '150px' }} />
+        <h2>Weather in {country.capital}</h2>
       </div>
     )
   }
@@ -42,7 +53,15 @@ const Countries = ({ searchResults }) => {
       return countrys.map(country => shortCountry(country))
     }
     else if (countrys.length === 1) {
-      return longCountry(countrys[0]);
+      return (
+        <>
+          {longCountry(countrys[0])}
+          <p>Temperature: {weather.temperature}</p>
+          <img src={weather.weather_icons} alt={weather.weather_icons} style={{ width: '50px' }} />
+          <p>Wind: {weather.wind_speed} kph direction {weather.wind_dir}</p>
+        </>
+      )
+
     }
   }
 
